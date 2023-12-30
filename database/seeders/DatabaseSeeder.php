3fc5ccb5800be3556630bde30c8071c45e374c16
sudharsandeveloper <sudharsandeveloper@users.noreply.github.com>
2023-12-30 19:49:29 +0530

    new seeder added for user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,8 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call(
             [
-                AreaSeeder::class
+                UserSeeder::class,
+                AreaSeeder::class,
             ]
         ); 
     }
